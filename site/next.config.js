4a0a9793c340ad633505e0fa8fc4cd6e7afac1cf
eatinsundip <43767555+eatinsundip@users.noreply.github.com>
2025-09-26 11:27:56 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   distDir: '../public',
   env: {
     name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    description: 'Collin\'s container store.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://eatinsundip.github.io/kasm-registry',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
